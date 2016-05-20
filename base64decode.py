5d72e5c8413c95c1d90c8fbac0051d220e4a7b88
JyhDoGG <875134940@qq.com>
2016-05-20 20:36:07 +0800

    Add files via upload

---
+++ base64decode.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+'SafeBase64Decode'
+
+__author='Jyh'
+
+import base64
+
+def safe_base64_decode(s):
+	n = len(s) % 4
+	if n == 0:
+		print(base64.urlsafe_b64decode(s))
+	else:
+		for i in range(4-n):
+			s = s + "="
+		print(base64.urlsafe_b64decode(s))
+
+s = input('input the str')
+safe_base64_decode(s)
